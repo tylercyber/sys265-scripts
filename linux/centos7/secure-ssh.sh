2f4d571b8bbb32825d1ffdc3d8d6c3fbdacdc1a8
tylercyber <tyler.larose@mymail.champlain.edu>
2019-02-27 13:56:01 -0500

    Organized Directories

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,7 @@
+# SECURE-SSH.SH
+# AUTHOR TYLERCYBER
+# Creates a new ssh user called $1
+# Adds a public key to that users authorized keys file
+# Removes roots ability to ssh in
+
+echo "testing 123"
